60ed18a3ef2037facc01e93d04c22a5453b82ca3
spittank <stefan.spittank@codecentric.de>
2022-02-01 11:12:30 +0100

    localhost via https

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+const path = require('path');
 const {defineConfig} = require('vite');
+const mkcert = require('vite-plugin-mkcert').default;
 const reactRefresh = require('@vitejs/plugin-react-refresh');
 
-// https://vitejs.dev/config/
 module.exports = defineConfig({
-  plugins: [reactRefresh()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html'),
+        app: path.resolve(__dirname, 'app.html'),
+      },
+    },
+  },
+  server: {
+    https: true,
+  },
+  plugins: [mkcert(), reactRefresh()],
 });
