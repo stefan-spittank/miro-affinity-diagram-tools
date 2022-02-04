ee3151b27bb649bf0ac859048a23475b16f92566
spittank <stefan.spittank@codecentric.de>
2022-02-04 12:52:35 +0100

    test env handling

--- src/sharedConsts.ts
+++ src/sharedConsts.ts
@@ -1,3 +1,6 @@
 export const appId = import.meta.env.VITE_MIRO_APP_ID || "3458764514442200539";
 
-console.log("appId", appId);
+console.log(
+  "import-meta-env-VITE_MIRO_APP_ID direct",
+  import.meta.env.VITE_MIRO_APP_ID
+);
